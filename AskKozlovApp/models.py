6ac93f09c6ee9a5f0f23dab8bfdcb8a20a6f56ac
FackOff25 <kozvladimir@bk.ru>
2022-06-12 12:45:23 +0300

    Titles fixed

--- AskKozlovApp/models.py
+++ AskKozlovApp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models import ObjectDoesNotExist
+from django.db.models import Sum, Count
+from django.http import HttpResponse, Http404
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    userPfp = models.CharField(max_length=255,
+                                  default=None)
+
+    def __str__(self):
+        if self.user.username == '':
+            return "ERROR-LOGIN IS NULL"
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'User profile'
+        verbose_name_plural = 'User profiles'
+
+    @property
+    def userlink(self):
+        return "/user/" + self.user.username
--- AskKozlovApp/views.py
+++ AskKozlovApp/views.py
@@ -16,14 +16,14 @@ def paginate(objects_list, request, per_page=5):
 def new_questions(request):
     request.user = fictionalDB.USER
     iterators = paginate(fictionalDB.QUESTIONS, request, 5)
-    return render(request, 'newquestions.html', {'title': "New", 'BestTags': fictionalDB.TAGS,
+    return render(request, 'newquestions.html', {'BestTags': fictionalDB.TAGS,
                                                  'BestUsers': fictionalDB.BESTUSERS, 'iterators': iterators})
 
 
 def hot_questions(request):
     request.user = fictionalDB.USER
     iterators = paginate(fictionalDB.QUESTIONS, request, 5)
-    return render(request, 'hotquestions.html', {'title': "Hot", 'BestTags': fictionalDB.TAGS,
+    return render(request, 'hotquestions.html', {'BestTags': fictionalDB.TAGS,
                                                  'BestUsers': fictionalDB.BESTUSERS, 'iterators': iterators})
 
 
@@ -31,44 +31,43 @@ def hot_questions(request):
 def list_with_tags(request, tg):
     request.user = fictionalDB.USER
     iterators = paginate(fictionalDB.QUESTIONS, request, 5)
-    return render(request, 'tag.html', {'title': tg, 'BestTags': fictionalDB.TAGS,
+    return render(request, 'tag.html', {'BestTags': fictionalDB.TAGS,
                                         'BestUsers': fictionalDB.BESTUSERS, 'iterators': iterators, 'header': tg})
 
 
 def signup(request):
-    return render(request, "registration.html", {'title': "Sign Up", 'BestTags': fictionalDB.TAGS,
+    return render(request, "registration.html", {'BestTags': fictionalDB.TAGS,
                                                  'BestUsers': fictionalDB.BESTUSERS})
 
 
 def login(request):
-    return render(request, "login.html", {'title': "Log In", 'BestTags': fictionalDB.TAGS,
+    return render(request, "login.html", {'BestTags': fictionalDB.TAGS,
                                           'BestUsers': fictionalDB.BESTUSERS})
 
 
 # for now, it is just link to main page
 def logout(request):
-    return render(request, "include/index.html", {'title': "Main Page", 'BestTags': fictionalDB.TAGS,
+    return render(request, "include/index.html", {'BestTags': fictionalDB.TAGS,
                                                   'BestUsers': fictionalDB.BESTUSERS,
                                                   'questions': fictionalDB.QUESTIONS})
 
 
 def settings(request):
     request.user = fictionalDB.USER
-    return render(request, "settings.html", {'title': "Settings", 'BestTags': fictionalDB.TAGS,
+    return render(request, "settings.html", {'BestTags': fictionalDB.TAGS,
                                              'BestUsers': fictionalDB.BESTUSERS})
 
 
 def ask(request):
     request.user = fictionalDB.USER
-    return render(request, "newquestion.html", {'title': "New Question", 'BestTags': fictionalDB.TAGS,
+    return render(request, "newquestion.html", {'BestTags': fictionalDB.TAGS,
                                                 'BestUsers': fictionalDB.BESTUSERS})
 
 
 def question(request, qid: int):
     request.user = fictionalDB.USER
     iterators = paginate(fictionalDB.ANSWERS, request, 5)
-    return render(request, "question.html", {'title': fictionalDB.QUESTIONS[qid]['title'],
-                                             'BestTags': fictionalDB.TAGS,
+    return render(request, "question.html", {'BestTags': fictionalDB.TAGS,
                                              'BestUsers': fictionalDB.BESTUSERS,
                                              'isAsker': True,
                                              'question': fictionalDB.QUESTIONS[qid],
